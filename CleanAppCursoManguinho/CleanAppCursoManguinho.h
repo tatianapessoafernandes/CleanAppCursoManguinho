5b3ab19eb60ab8c00b933afdcd7ad1257b10658c
Tatiana <tatianapeople@yahoo.com.br>
2022-06-22 15:00:15 -0300

    Initial Commit

---
+++ CleanAppCursoManguinho/CleanAppCursoManguinho.h
@@ -0,0 +1,18 @@
+//
+//  CleanAppCursoManguinho.h
+//  CleanAppCursoManguinho
+//
+//  Created by Tatiana Fernandes | Gerencianet on 22/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CleanAppCursoManguinho.
+FOUNDATION_EXPORT double CleanAppCursoManguinhoVersionNumber;
+
+//! Project version string for CleanAppCursoManguinho.
+FOUNDATION_EXPORT const unsigned char CleanAppCursoManguinhoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CleanAppCursoManguinho/PublicHeader.h>
+
+
